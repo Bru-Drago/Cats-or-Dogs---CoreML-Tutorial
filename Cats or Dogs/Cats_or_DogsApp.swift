7df67386d81a5c3d233d3b89b9c94a01c96f44a9
Bru-Drago <bruna_drago@hotmail.com>
2020-12-03 13:50:08 -0300

    Initial Commit

---
+++ Cats or Dogs/Cats_or_DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cats_or_DogsApp.swift
+//  Cats or Dogs
+//
+//  Created by Bruna Fernanda Drago on 03/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Cats_or_DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
